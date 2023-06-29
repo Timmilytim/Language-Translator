15e8f04cf2be7e57d1eb66dc80d85200505c9bc4
iamunik <blessedcoolemma@gmail.com>
2023-06-29 01:06:49 +0100

    Little updates made

--- LanguageTranslator.py
+++ LanguageTranslator.py
@@ -1,359 +1,100 @@
+import googletrans
+import httpcore
 from googletrans import Translator
-import json
-import requests
+import time
+
+# The function below allows users to search based on the language key and the language itself then it translates
+# It also checks if the language key exists in the key list and if the language is in the value list
+def trans_index(language, value, keys, sentence):
+    if len(language) > 2:
+        if language in value:
+            idx = value.index(language)
+            key = keys[idx]
+
+            trans_late = translator.translate(sentence, dest=key).text
+            print("Long Language", key)
+            print(trans_late)
+
+        else:
+            print("\nThe selected Language doesn't exist\n"
+                  "Check the available language in the 'AVAILABLE LANGUAGE CODES' section.\n")
+    else:
+        if language in keys:
+            key = language
+            trans_late = translator.translate(sentence, dest=key).text
+            print(trans_late)
+
+        else:
+            print("\nThe selected language key doesn't exist\n"
+                  "Check the available language code in the 'AVAILABLE LANGUAGE CODES' section.\n")
 
 
-def timi():
-	try:
-		print("------ WELCOME TO TIMMY'S LANGUAGE TRANSLATOR------")
-		while True:
-			option = input("1. TRANSLATOR\n2. AVAILABLE LANGUAGE CODES\n3. Exit\n-> ")
+def main():
+    if option == 1:
+        speaker = input("Are you an English speaker? - YES or NO -\n-> ").lower()
+        try:
+            if speaker == "yes":  # Speaker understands english
+                sentence = input("Enter a sentence to be translated.\n-> ")
+                language = input("Enter your preferred translation.\n-> ").lower()
+                trans_index(language, Value, Keys, sentence)
 
-			if option == "1":		# First Option for the Translation
-				try:
-					url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+            elif speaker == "no":  # Speaker does not understand english
+                sentence = input("Enter a sentence to be translated.\n-> ")
+                de_tect = translator.detect(sentence)
+                if de_tect.lang in Keys:
+                    idx = Keys.index(de_tect.lang)
+                    language = Value[idx]
+                    print(f"Detected language is {language.upper()}")
+                else:
+                    print('Language not detected!!')
+                language = input("Enter your preferred translation.\n-> ").lower()
+                trans_index(language, Value, Keys, sentence)
+            select = input("Do you want to make another translation? - YES - NO -\n-> ")
+            if select == "yes":
+                pass
+            else:
+                print("Exiting program....")
+                time.sleep(1)
+                exit(0)
+        except httpcore.ConnectError:
+            print("\nPLEASE CONNECT TO THE INTERNET AND RERUN THE PROGRAM!!!!")
+            time.sleep(1)
+            exit(0)
 
-					sentence = str(input("Enter Sentence: "))
-					language = str(input("Enter Language Code: "))
-					payload = {
-						"q": sentence,
-						"target": language,
-						"source": "en"
-					}
-					headers = {
-						"content-type": "application/x-www-form-urlencoded",
-						"Accept-Encoding": "application/gzip",
-						"X-RapidAPI-Key": "16d35def64msh61682f7697de9a3p1ec241jsnedffd87d0759",
-						"X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
-					}
-					response = requests.request("GET", url, headers=headers, params=sentence)
-					response = requests.post(url, data=payload, headers=headers)
-					betterView = json.loads(response.text)["data"]["translations"][0]["translatedText"]
+    elif option == 2:
+        print(f"Available Languages are {len(Keys)}\nPlease wait for the available languages to load...\n")
+        time.sleep(2)
+        for i in range(len(Keys)):
+            print(f"{Keys[i]} -------- {Value[i]}")
+            time.sleep(0.05)
+        print(f"\nA total of {len(Keys)} languages....")
+        time.sleep(1)
 
-					print("TRANSLATION: ", betterView, " \n")
-				except KeyError:
-					print("LANGUAGE NOT AVAILABLE....\n",
-						  "GO TO THE 'LANGUAGE CODES' OPTION BELOW TO CHECK OUT THE CODES AVAILABLE LANGUAGES.... \n" )
+    elif option == 3:
+        print("Exiting program....")
+        exit(0)
 
-			elif option == "2":		# Available Language code
-				print('''SOME JARGONS I'M STILL WORKING ON...\n\n
-				''')
-
-			elif option == "3":		# Exit option
-				print("Exiting...")
-				exit(0)
-
-			else:
-				print("WRONG INPUT \n"
-					  "TRY AGAIN.... \n")
-	except requests.exceptions.ConnectionError:
-		print("Make sure you are Connected to the internet...")
-
-
-"""
-BEGINNING OF PROGRAM
-	Welcome user to our translation portal
-	Then ask what they wanna do
-		1. Translate
-		2. View Available Langs
-		3. Exit
-
-if input is 1 (Translate)...
-	- Ask if the individual is an english speaker if yes...
-		- FOR TRANSLATION FROM ENGLISH:
-			- Collect user input in ENGLISH
-			- Ask user to type in the Language the want to translate to
-				- if preferred lang is > 2
-				- preferred lang.lower() then check if Language exists in the list of Languages
-				- If it exists in the lang.values() get the corresponding key in the lang.Keys()
-			- Then perform translation
-
-	- If individual isn't an English speaker			
-		- FOR DETECTING LANGUAGES (use the .detect()):
-			- Collect user input in desired language
-			- Ask user to type in the Language the want to translate to
-			- Check if Language exists in the list of Languages
-			- If it does perform translation
-
-if input is 2 (View available Langs)...
-	- Prompt user that the available languages are len(Keys) in total
-	- Print loading language translations
-	- Use a time.sleep(2)
-	- Print the available langs and their keys
-	- Tell user that they can also use the Keys directly
-"""
+    else:
+        print("Incorrect input please check for the correct option and try again!!!")
 
 
-def OG():
-	translator = Translator()
-	LANGUAGES = {
-		'af': 'afrikaans',
-		'sq': 'albanian',
-		'am': 'amharic',
-		'ar': 'arabic',
-		'hy': 'armenian',
-		'az': 'azerbaijani',
-		'eu': 'basque',
-		'be': 'belarusian',
-		'bn': 'bengali',
-		'bs': 'bosnian',
-		'bg': 'bulgarian',
-		'ca': 'catalan',
-		'ceb': 'cebuano',
-		'ny': 'chichewa',
-		'zh-cn': 'chinese (simplified)',
-		'zh-tw': 'chinese (traditional)',
-		'co': 'corsican',
-		'hr': 'croatian',
-		'cs': 'czech',
-		'da': 'danish',
-		'nl': 'dutch',
-		'en': 'english',
-		'eo': 'esperanto',
-		'et': 'estonian',
-		'tl': 'filipino',
-		'fi': 'finnish',
-		'fr': 'french',
-		'fy': 'frisian',
-		'gl': 'galician',
-		'ka': 'georgian',
-		'de': 'german',
-		'el': 'greek',
-		'gu': 'gujarati',
-		'ht': 'haitian creole',
-		'ha': 'hausa',
-		'haw': 'hawaiian',
-		'iw': 'hebrew',
-		'he': 'hebrew',
-		'hi': 'hindi',
-		'hmn': 'hmong',
-		'hu': 'hungarian',
-		'is': 'icelandic',
-		'ig': 'igbo',
-		'id': 'indonesian',
-		'ga': 'irish',
-		'it': 'italian',
-		'ja': 'japanese',
-		'jw': 'javanese',
-		'kn': 'kannada',
-		'kk': 'kazakh',
-		'km': 'khmer',
-		'ko': 'korean',
-		'ku': 'kurdish (kurmanji)',
-		'ky': 'kyrgyz',
-		'lo': 'lao',
-		'la': 'latin',
-		'lv': 'latvian',
-		'lt': 'lithuanian',
-		'lb': 'luxembourgish',
-		'mk': 'macedonian',
-		'mg': 'malagasy',
-		'ms': 'malay',
-		'ml': 'malayalam',
-		'mt': 'maltese',
-		'mi': 'maori',
-		'mr': 'marathi',
-		'mn': 'mongolian',
-		'my': 'myanmar (burmese)',
-		'ne': 'nepali',
-		'no': 'norwegian',
-		'or': 'odia',
-		'ps': 'pashto',
-		'fa': 'persian',
-		'pl': 'polish',
-		'pt': 'portuguese',
-		'pa': 'punjabi',
-		'ro': 'romanian',
-		'ru': 'russian',
-		'sm': 'samoan',
-		'gd': 'scots gaelic',
-		'sr': 'serbian',
-		'st': 'sesotho',
-		'sn': 'shona',
-		'sd': 'sindhi',
-		'si': 'sinhala',
-		'sk': 'slovak',
-		'sl': 'slovenian',
-		'so': 'somali',
-		'es': 'spanish',
-		'su': 'sundanese',
-		'sw': 'swahili',
-		'sv': 'swedish',
-		'tg': 'tajik',
-		'ta': 'tamil',
-		'te': 'telugu',
-		'th': 'thai',
-		'tr': 'turkish',
-		'uk': 'ukrainian',
-		'ur': 'urdu',
-		'ug': 'uyghur',
-		'uz': 'uzbek',
-		'vi': 'vietnamese',
-		'cy': 'welsh',
-		'xh': 'xhosa',
-		'yi': 'yiddish',
-		'yo': 'yoruba',
-		'zu': 'zulu'}
+translator = Translator()
+LANGUAGES = googletrans.LANGUAGES
 
-	Keys = list(LANGUAGES.keys())
-	Value = list(LANGUAGES.values())
+Keys = list(LANGUAGES.keys())
+Value = list(LANGUAGES.values())
 
-	option = int(input("""
+checks = True
+
+while True:
+    try:
+        option = int(input("""
 _____ WELCOME TO TIMI'S LANGUAGE TRANSLATOR _____
-	1. TRANSLATE
-	2. AVAILABLE LANGUAGE CODES
-	3. EXIT"""))
-
-	if option == 1:
-		speaker = input("Are you an English speaker?\n-> ")
-		if speaker.lower() == "yes":
-			sentence = input("Enter a sentence to be translated.\n-> ")
-			language = input("Enter your preferred translation.\n-> ").lower()
-			# print(len(language))
-			if len(language) > 2:
-				if language in Value:
-					idx = Value.index(language)
-					key = Keys[idx]
-					# print(key)
-					# Translation goes here
-				else:
-					print("Doesn't exist")
-			else:
-				if language in Keys:
-					key = language
-					# print(key)
-					# Translation goes here
-				else:
-					print("Doesn't exist")
-		elif speaker.lower() == "no":
-			"""LANGUAGE DETECT"""
-			# use the translator.detect()
-
-
-
-
-
-# for i in range(len(Keys)):
-	# print(list(Keys)[i], "---", list(Value)[i])
-
-# print(len(Keys))
-
-# OG()
-
-# LANGUAGES = {
-# 		'af': 'afrikaans',
-# 		'sq': 'albanian',
-# 		'am': 'amharic',
-# 		'ar': 'arabic',
-# 		'hy': 'armenian',
-# 		'az': 'azerbaijani',
-# 		'eu': 'basque',
-# 		'be': 'belarusian',
-# 		'bn': 'bengali',
-# 		'bs': 'bosnian',
-# 		'bg': 'bulgarian',
-# 		'ca': 'catalan',
-# 		'ceb': 'cebuano',
-# 		'ny': 'chichewa',
-# 		'zh-cn': 'chinese (simplified)',
-# 		'zh-tw': 'chinese (traditional)',
-# 		'co': 'corsican',
-# 		'hr': 'croatian',
-# 		'cs': 'czech',
-# 		'da': 'danish',
-# 		'nl': 'dutch',
-# 		'en': 'english',
-# 		'eo': 'esperanto',
-# 		'et': 'estonian',
-# 		'tl': 'filipino',
-# 		'fi': 'finnish',
-# 		'fr': 'french',
-# 		'fy': 'frisian',
-# 		'gl': 'galician',
-# 		'ka': 'georgian',
-# 		'de': 'german',
-# 		'el': 'greek',
-# 		'gu': 'gujarati',
-# 		'ht': 'haitian creole',
-# 		'ha': 'hausa',
-# 		'haw': 'hawaiian',
-# 		'iw': 'hebrew',
-# 		'he': 'hebrew',
-# 		'hi': 'hindi',
-# 		'hmn': 'hmong',
-# 		'hu': 'hungarian',
-# 		'is': 'icelandic',
-# 		'ig': 'igbo',
-# 		'id': 'indonesian',
-# 		'ga': 'irish',
-# 		'it': 'italian',
-# 		'ja': 'japanese',
-# 		'jw': 'javanese',
-# 		'kn': 'kannada',
-# 		'kk': 'kazakh',
-# 		'km': 'khmer',
-# 		'ko': 'korean',
-# 		'ku': 'kurdish (kurmanji)',
-# 		'ky': 'kyrgyz',
-# 		'lo': 'lao',
-# 		'la': 'latin',
-# 		'lv': 'latvian',
-# 		'lt': 'lithuanian',
-# 		'lb': 'luxembourgish',
-# 		'mk': 'macedonian',
-# 		'mg': 'malagasy',
-# 		'ms': 'malay',
-# 		'ml': 'malayalam',
-# 		'mt': 'maltese',
-# 		'mi': 'maori',
-# 		'mr': 'marathi',
-# 		'mn': 'mongolian',
-# 		'my': 'myanmar (burmese)',
-# 		'ne': 'nepali',
-# 		'no': 'norwegian',
-# 		'or': 'odia',
-# 		'ps': 'pashto',
-# 		'fa': 'persian',
-# 		'pl': 'polish',
-# 		'pt': 'portuguese',
-# 		'pa': 'punjabi',
-# 		'ro': 'romanian',
-# 		'ru': 'russian',
-# 		'sm': 'samoan',
-# 		'gd': 'scots gaelic',
-# 		'sr': 'serbian',
-# 		'st': 'sesotho',
-# 		'sn': 'shona',
-# 		'sd': 'sindhi',
-# 		'si': 'sinhala',
-# 		'sk': 'slovak',
-# 		'sl': 'slovenian',
-# 		'so': 'somali',
-# 		'es': 'spanish',
-# 		'su': 'sundanese',
-# 		'sw': 'swahili',
-# 		'sv': 'swedish',
-# 		'tg': 'tajik',
-# 		'ta': 'tamil',
-# 		'te': 'telugu',
-# 		'th': 'thai',
-# 		'tr': 'turkish',
-# 		'uk': 'ukrainian',
-# 		'ur': 'urdu',
-# 		'ug': 'uyghur',
-# 		'uz': 'uzbek',
-# 		'vi': 'vietnamese',
-# 		'cy': 'welsh',
-# 		'xh': 'xhosa',
-# 		'yi': 'yiddish',
-# 		'yo': 'yoruba',
-# 		'zu': 'zulu'}
-
-""" """
-# seun = "google.com"
-# lst = [i for i in seun]
-# nlst = set(lst)
-# for i in nlst:
-# 	print(i, "-----", lst.count(i))
-
-# print(nlst)
+1. TRANSLATE
+2. AVAILABLE LANGUAGE CODES
+3. EXIT
+-> """))
+        main()
+    except ValueError:
+        print("\nIncorrect input please check for the correct option and try again!!!")
+        time.sleep(1)
